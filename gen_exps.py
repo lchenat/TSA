eb90e55b095b9c7f35245853d8ec581ffb07513b
lchenat <lchenat@connect.ust.hk>
2019-04-02 23:56:55 -0700

    actor mimic search

--- gen_exps.py
+++ gen_exps.py
@@ -169,6 +169,34 @@ def nineroom_finetune_search(feat_dim):
         random.shuffle(cmds)
         f.writelines(cmds)
 
+@cmd()
+def nineroom_actor_mimic_search(base_dir, feat_dim, touch=True):
+    exp_path = Path('exps/pick/nineroom/actor_mimic_search_{}'.format(feat_dim))
+    kwargs = { 
+        '--agent': 'tsa',
+        '--env_config': 'data/env_configs/pick/nineroom/nineroom.8',
+        '--net': 'baseline',
+        '--visual': 'mini',
+        '--gate': 'softplus',
+        '--feat_dim': int(feat_dim),
+        '--load_part': 'abs',
+        '--obs_type': 'mask',
+        '--scale': 2,
+        '--eval_interval': 15, 
+        '--save_interval': 1,
+        '--steps': 500000,
+    }
+    if touch: open(exp_path, 'w').close()
+    with open(exp_path, 'a+') as f:
+        for name in os.listdir(base_dir):
+            step = int(name.split('-')[1])
+            if step <= 800000: continue
+            for seed in range(3):
+                kwargs['--weight'] = Path(base_dir, name)
+                kwargs['--tag'] = 'actor_mimic_{}-{}'.format(feat_dim, step)
+                kwargs['--seed'] = seed
+                dump_args(f, kwargs=kwargs)
+
 
 if __name__ == "__main__":
     cmd_run()
