36c4affdaf48b02586bf39b865c68bfbb03b9b94
lchenat <lchenat@connect.ust.hk>
2019-05-04 17:08:05 -0700

    run sarsa

--- deep_rl/agent/Sarsa_agent.py
+++ deep_rl/agent/Sarsa_agent.py
@@ -6,33 +6,6 @@ from .BaseAgent import *
 from itertools import chain
 from collections import defaultdict
 
-def softmax(a):
-    a = np.exp(a - a.max())
-    return a / a.sum(-1)
-
-class SarsaActor(BaseActor):
-    def __init__(self, config):
-        BaseActor.__init__(self, config)
-        self.config = config
-        self.start()
-
-    def _transition(self):
-        if self._state is None:
-            self._state = self._task.reset()
-            self._info = self._task.get_info()
-        config = self.config
-        q_values = self._network(config.state_normalizer(self._state), self._info)
-        q_values = to_np(q_values).flatten()
-        if np.random.rand() < config.random_action_prob():
-            action = np.random.randint(0, len(q_values))
-        else:
-            action = np.argmax(q_values)
-        next_state, reward, done, info = self._task.step([action])
-        entry = [self._state[0], action, reward[0], next_state[0], int(done[0]), self._info, info]
-        self._total_steps += 1
-        self._state = next_state
-        self._info = info
-        return entry
 
 class SarsaAgent(BaseAgent):
     def __init__(self, config):
@@ -44,7 +17,7 @@ class SarsaAgent(BaseAgent):
         self.episode_reward = 0
         self.episode_rewards = []
         self.total_steps = 0
-        self.state = self.task.reset()
+        self.state = config.state_normalizer(self.task.reset())
         self.info = self.task.get_info()
         self.action = self.select_action()
 
@@ -61,7 +34,7 @@ class SarsaAgent(BaseAgent):
 
     def select_action(self):
         config = self.config
-        q_values = self.network(config.state_normalizer(self.state), self.info)
+        q_values = self.network(self.state, self.info)
         q_values = to_np(q_values).flatten()
         if np.random.rand() < config.random_action_prob():
             action = np.random.randint(0, len(q_values))
@@ -81,7 +54,7 @@ class SarsaAgent(BaseAgent):
                 self.episode_rewards.append(self.episode_reward)
                 self.episode_reward = 0
             q = self.network(self.state, self.info)[:1, self.action]
-            next_q = (reward + config.discount * done * self.network(next_state, next_info)[:1, next_action]).detach()
+            next_q = (float(reward) + float(config.discount * (1 - done)) * self.network(next_state, next_info)[:1, next_action]).detach()
             loss = F.mse_loss(q, next_q)
             loss_dict['MSE'].append(loss)
             self.optimizer.zero_grad()
